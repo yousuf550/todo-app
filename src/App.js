a23795ff959add8fff070507b10ac3b522ebb4cd
Muhammad Yousuf <yousuf.hanif550@gmail.com>
2022-08-14 14:53:01 +0500

    Update Todo App
    Create Todos, TodoItem & AddTodo Component & logic for add and delete todos

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./Components/Header";
 import { Todos } from "./Components/Todos";
+import AddTodo  from "./Components/AddTodo";
 
 function App() {
+
+  const onDelete = (todo) => {
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }))
+  }
+
+  const addTodo = (title, desc) =>{
+    let sno;
+    if(todos.length===0){
+      sno=0;
+    }
+    else{
+      sno = todos[todos.length-1].sno +1
+    }
+
+    const newTodo = {
+      sno: sno,
+      title:title,
+      desc:desc,
+    }
+    setTodos([...todos, newTodo])
+  }
+
+  const [todos, setTodos] = useState([]);
+
   return (
     <>
-      <Header title="Todo List App" searchBar={false} />
-      <Todos />
+      <Header title="Todo App" searchBar={false} />
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos={todos} onDelete={onDelete} />
     </>
   );
 }
--- src/Components/Todos.js
+++ src/Components/Todos.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import { TodoItem } from "./TodoItem"
 
-export const Todos = () => {
+export const Todos = (props) => {
   return (
-    <div>Todos Work</div>
+    <div className='container'>
+      <h3 className='my-4'>Todos List</h3>
+
+      {
+        props.todos.length === 0 ? "No Todos to display" :
+          props.todos.map((todo) => {
+            return (
+              <>
+                <TodoItem todo={todo} onDelete={props.onDelete} key={todo.sno} />
+                <hr />
+              </>
+            )
+          })
+      }
+    </div>
   )
 }
